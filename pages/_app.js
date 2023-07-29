6add1ac0f0c7d4b3df7d0cbd4d0f7ba42d7c87a7
Aviral Dhingra <aviral.dhingra.2008@gmail.com>
2023-07-29 20:04:06 +0530

    Upload...

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
-import '@/css/tailwind.css'
+import Head from 'next/head'
+import Script from 'next/script'
+
 import '@/css/prism.css'
+import '@/css/tailwind.css'
 import 'katex/dist/katex.css'
 
 import '@fontsource/inter/variable-full.css'
 
 import { ThemeProvider } from 'next-themes'
-import Head from 'next/head'
 
-import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
-import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+import LayoutWrapper from '@/components/LayoutWrapper'
+import Analytics from '@/components/analytics'
+import siteMetadata from '@/data/siteMetadata'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
@@ -18,6 +20,21 @@ const isSocket = process.env.SOCKET
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
